1d47473596b550c80a8689ac5e0ee1d49770c5d7
losingways <707602696@qq.com>
2024-03-17 11:26:08 +0800

    update

--- src/learn/Internet/Tcp/demo01/TcpClientDemo01.java
+++ src/learn/Internet/Tcp/demo01/TcpClientDemo01.java
@@ -13,10 +13,13 @@ public class TcpClientDemo01 {
         Socket socket = null;
         InetAddress serverIp = null;
         try {
+            //从键盘获取服务器IP地址
             serverIp = InetAddress.getByName("127.0.0.1");
-            socket = new Socket(serverIp, 9999);
-            
+            //创建socket对象(用来获取服务端的接口)，需要指定服务器的地址和端口号
+            socket = new Socket(serverIp, 9999); 
+            //获取端口对应的输出流
             os = socket.getOutputStream();
+            //向服务器发送消息
             os.write("你好，服务器".getBytes());
             
 
--- src/learn/Internet/Tcp/demo01/TcpServerDemon01.java
+++ src/learn/Internet/Tcp/demo01/TcpServerDemon01.java
@@ -9,19 +9,30 @@ import java.net.Socket;
 //服务端
 public class TcpServerDemon01 {
     public static void main(String[] args) {
+        //定义ByteArrayOutputStream对象，用于接收客户端数据
         ByteArrayOutputStream baos = null;
+        //定义InputStream对象，用于接收客户端数据
         InputStream is = null;
+        //定义ServerSocket对象，用于监听客户端连接
         ServerSocket serverSocket = null;
         try {
+            //创建ServerSocket对象，监听9999端口
             serverSocket = new ServerSocket(9999);
+            //accept方法返回一个Socket对象，表示客户端已连接
             Socket accept = serverSocket.accept();
             System.out.println("客户端已连接");
+            //获取客户端的输入流
             is = accept.getInputStream();
 
             baos = new ByteArrayOutputStream();
+            //创建一个ByteArrayOutputStream对象，用于存储客户端发送的数据
+            //定义一个byte数组，用于接收客户端数据
             byte[] buffer = new byte[1024];
+            //定义一个int类型的变量，用于接收客户端发送的数据长度
             int len;
+            //接收客户端数据
             while ((len = is.read(buffer)) != -1) {
+                //将接收到的数据存入ByteArrayOutputStream对象中
                 baos.write(buffer, 0, len);
             }
             System.out.println(baos.toString());
@@ -30,6 +41,7 @@ public class TcpServerDemon01 {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            //释放资源
             if(!is.equals(null))
                 try {
                     is.close();
